888175414d4d7258a352d2eb705b299b3ea456c2
Cam-Black <cameronablack1998@gmail.com>
2023-04-24 11:17:22 +0100

    Added main method to utilise Callable interface implemented in Adder class and utilised Future interface to store values while waiting for thread work to complete.

--- src/main/java/org/example/CallableFuture.java
+++ src/main/java/org/example/CallableFuture.java
@@ -1,4 +1,47 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.adder.Adder;
+import org.example.files.InputFiles;
+import org.example.timing.Timing;
+
+import java.util.Arrays;
+import java.util.concurrent.*;
+
 public class CallableFuture {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting multi-threaded application");
+        Timing.startTime();
+
+        ExecutorService es = Executors.newFixedThreadPool(3);
+        Future<?>[] results = new Future[InputFiles.getFilesLength()];
+
+        for (int i = 0; i < InputFiles.getFilesLength(); i++) {
+            Adder adder = new Adder(InputFiles.getFile(i));
+            results[i] = es.submit(adder);
+        }
+
+        Arrays.stream(results).forEach(el -> {
+            try {
+                LOGGER.info("Total: {}", el.get());
+            } catch (InterruptedException | ExecutionException e) {
+                LOGGER.error(e);
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        try {
+            es.shutdown();
+            if (es.awaitTermination(60, TimeUnit.SECONDS)) {
+                Timing.endTime();
+                Timing.getElapsedTime();
+            }
+        } catch (InterruptedException ie) {
+            LOGGER.error(ie);
+            Thread.currentThread().interrupt();
+        }
+    }
 }
