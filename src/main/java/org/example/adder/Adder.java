48aacc6c40ecf45a493bc4913f2f82521e1ad42d
Cam-Black <cameronablack1998@gmail.com>
2023-04-24 11:16:21 +0100

    Changed Adder class to just read files and return a value of the total number of lines.

--- src/main/java/org/example/adder/Adder.java
+++ src/main/java/org/example/adder/Adder.java
@@ -1,44 +1,43 @@
 package org.example.adder;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
-public class Adder implements Runnable {
+public class Adder implements Callable<Integer> {
     private static final Logger LOGGER = LogManager.getLogger();
     private final String inFile;
-    private final String outFile;
 
-    public Adder(String inFile, String outFile) {
+    public Adder(String inFile) {
         super();
         this.inFile = inFile;
-        this.outFile = outFile;
     }
 
-    public void doAdd() throws IOException {
-        long total = 0;
-
+    public int doAdd() {
+        int total = 0;
         try (BufferedReader br = Files.newBufferedReader(Paths.get(inFile))) {
-            while (br.readLine() != null) {
-                total += br.lines().count();
-            }
-        }
-        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(outFile))) {
-            bw.write("Total: " + total);
-        }
-    }
+            List<String> lines = br.lines().collect(Collectors.toList());
 
-    @Override
-    public void run() {
-        try {
-            doAdd();
+            if (CollectionUtils.isNotEmpty(lines)) {
+                total += lines.size();
+            }
+
         } catch (IOException ioe) {
             LOGGER.error(ioe);
         }
+        return total;
+    }
+
+    @Override
+    public Integer call() throws IOException {
+            return doAdd();
     }
 }
